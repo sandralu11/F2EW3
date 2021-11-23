e807890c4704069a6dc60f151b658536cd8a18f1
Sandra <a0960529965@gmail.com>
2021-11-23 18:03:26 +0800

    home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="btn">
+        <div>
+          <img src="../assets/bus.png" alt="">
+          <div>
+            <h2>搜尋公車</h2>
+            <p>輸入公車號，查找公車路線</p>
+          </div>
+        </div>
+        <i class='bx bx-chevron-right'></i>
+    </div>
   </div>
 </template>
 
@@ -16,3 +24,20 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+@import '../assets/share.scss';
+.home{
+  background-image:url("https://source.unsplash.com/random/1440x800/?bus");
+  height: 100vh;
+  width: 100%;
+  .btn{
+    background-color: #fff;
+    h2{
+      color: $text-color;
+      font-size: $title-font-size;
+    }
+  }
+}
+
+</style>
