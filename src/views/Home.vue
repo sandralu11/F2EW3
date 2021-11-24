3530e7c9c55289cb3667df39e3b42abfad7c52ca
Sandra lu <a0960529965@gmail.com>
2021-11-24 23:34:18 +0800

    index

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,29 @@
 <template>
   <div class="home">
-    <div class="btn">
-        <div>
-          <img src="../assets/bus.png" alt="">
-          <div>
-            <h2>搜尋公車</h2>
-            <p>輸入公車號，查找公車路線</p>
-          </div>
+    <div class="header">
+      <img src="../assets/head.png" alt="">
+    </div>
+    <div class="btn-group">
+      <router-link to="/Search">
+        <div class="btn">
+            <img src="../assets/bus.png" alt="">
+              <div class="btn-box">
+              <h2>搜尋公車</h2>
+                <p>輸入公車號</p>
+            </div>
+            <i class='bx bx-chevron-right'></i>
         </div>
-        <i class='bx bx-chevron-right'></i>
+      </router-link>
+     
+      <div class="btn">
+          <img src="../assets/nearby.png" alt="">
+            <div class="btn-box">
+            <h2>附近站牌</h2>
+              <p>搜尋您附近的公車車站</p>
+          </div>
+          <i class='bx bx-chevron-right'></i>
+      </div>
+     
     </div>
   </div>
 </template>
@@ -31,11 +46,40 @@ export default {
   background-image:url("https://source.unsplash.com/random/1440x800/?bus");
   height: 100vh;
   width: 100%;
+  a{
+    
+  }
+  .header{
+    background: rgba(255, 255, 255, 0.4);
+  }
+  .btn-group{
+    width: 90%;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%,-50%);
+  }
   .btn{
     background-color: #fff;
+    width: 100%;
+    border-radius: 20px;
+    box-shadow: 0px 2px 8px rgba(111, 137, 162, 0.2);
+    padding: 20px;
+    margin-top:10px;
+    box-sizing: border-box;
+    display: flex;
+    justify-content: space-between;
+    .btn-box{
+      display: inline;
+      padding: 5px;
+    }
     h2{
       color: $text-color;
       font-size: $title-font-size;
+      font-weight: bold;
+    }
+    i{
+      line-height: 66px;
     }
   }
 }
---
+++ src/views/Search.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="bus-search">
+        <div class="head">
+            <router-link to="/">
+                <i class='bx bx-chevron-left'></i>
+            </router-link>
+            <div class="search-box">
+                <i class='bx bx-search'></i>
+                <input type="search" placeholder="今天要搭哪輛公車呢？">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../assets/share.scss';
+.bus-search{
+    background-color: $bg-color;
+    height: 100vh;
+    .head{
+        background-color: #fff;
+        padding: 10px;
+        box-shadow:0  1px #aaa;
+    }
+    .search-box{
+        display: inline-block;
+        width: 80%;
+        position: relative;
+        .bx-search{
+            position: absolute;
+            left: 15px;
+            top:10px;
+        }
+        input{
+            margin-left:5px ;
+            background-color: #F3F3F3;
+            border-radius: 20px;
+            padding: 10px 0 10px 30px ;
+            border: none;   
+            width: 100%;
+        }
+    }
+}
+</style>
